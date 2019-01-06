15f3e856feefcd04850ea5261e7a0baf142b4d3c
蔡家宝 <cjb0331@163.com>
2019-01-06 23:58:12 +0800

    新增接口白名单，url地址白名单 ip白名单 路由转发包含前缀等功能

--- zuul/src/main/java/com/ald/cloud/zuul/filter/AccessFilter.java
+++ zuul/src/main/java/com/ald/cloud/zuul/filter/AccessFilter.java
@@ -4,8 +4,12 @@ import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @program: aplus-cloud
@@ -34,11 +38,42 @@ public class AccessFilter extends ZuulFilter {
         return true;
     }
 
+    @Value("${routesurl}")
+    private String urllist;
+
     @Override
     public Object run() {
+
+
         RequestContext ctx = RequestContext.getCurrentContext();
         HttpServletRequest request = ctx.getRequest();
+//        ip白名单验证
+        String ipAddr = this.getIpAddr(request);
 
+        log.info("请求IP地址为：[{}]", ipAddr);
+        //配置本地IP白名单，生产环境可放入数据库或者redis中
+        List<String> ips = new ArrayList<String>();
+        ips.add("127.0.0.1");
+        ips.add("0:0:0:0:0:0:0:1");
+
+        if (!ips.contains(ipAddr)) {
+            log.info("IP地址校验不通过！！！");
+            ctx.setResponseStatusCode(401);
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseBody("IpAddr is forbidden!");
+            return ctx;
+        }
+        log.info("IP校验通过。");
+        //接口白名单
+        String requrl = request.getServletPath();
+        List<String> listStrings = Arrays.asList(urllist.split(","));
+        if (listStrings.contains(requrl)) {
+            log.info("接口白名单校验通过");
+            return null;
+        }
+        log.info("接口白名单校验失败，需要进行token校验");
+
+        //token校验
         log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
 
         Object accessToken = request.getParameter("accessToken");
@@ -51,6 +86,35 @@ public class AccessFilter extends ZuulFilter {
         log.info("access token ok");
         return null;
     }
+
+
+    /**
+     * 获取Ip地址
+     *
+     * @param request
+     * @return
+     */
+    public String getIpAddr(HttpServletRequest request) {
+
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+
 
 }
 
